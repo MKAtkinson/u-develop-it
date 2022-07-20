1fdfcccc688ca33b6db4e42dee00189462b088be
MKAtkinson <maxatkinson@ymail.com>
2022-07-20 16:35:32 -0500

    DB query connected

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
+const mysql = require('mysql2')
 app.use(express.urlencoded ({ extended: false}))
 app.use (express.json())
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'ThePacket145!',
+        database: 'election'
+    },
+    console.log('Connected to election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+})
 
 app.get((req,res) => {
     res.status(404).end()
